8c05bea1d23c07d4774137900580a30e375cbb79
Lucas Coraça Silva <lucascoraca@Lucass-MacBook-Pro.local>
2018-02-13 13:17:44 -0200

    more fixes

--- media.py
+++ media.py
@@ -4,13 +4,13 @@
 
 class Video():
     """class to keep video information
-
-       Attributes:
-            attr1(str): Video title
-            attr2(str): Video URL
     """
 
     def __init__(self, video_title, video_url):
+        """Attributes:
+             attr1(str): Video title
+             attr2(str): Video URL
+        """
         self.title = video_title
         self.trailer_youtube_url = video_url
 
@@ -19,15 +19,14 @@ class Video():
 
 class Movie(Video):
     """class to keep movie information
-
-       Attributes:
-            attr1(str): Video title
-            attr2(str): Video URL
-            attr3(str): Movie storyline
-            attr4(str): Poster image URL
     """
 
     def __init__(self, video_title, video_url, movie_storyline, poster_image):
+        """Attributes:
+             attr1(str): Video title
+             attr2(str): Video URL
+             attr3(str): Movie storyline
+             attr4(str): Poster image URL
+        """
         Video.__init__(self, video_title, video_url,)
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
